e32903b7a0059a85781c9aef04ae9aea09aa1ecb
Raymond Burrage <ray.inc.05@gmail.com>
2021-01-19 19:41:52 -0600

    nooooo

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PerseverenceTeleop.java
@@ -0,0 +1,176 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode;
+
+//import com.arcrobotics.ftclib.geometry.Pose2d;
+//import com.arcrobotics.ftclib.geometry.Rotation2d;
+//import com.arcrobotics.ftclib.geometry.Transform2d;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.teamcode.HardwarePerseverence;
+//import com.spartronics4915.lib.T265Camera;
+
+
+/**
+ * This OpMode uses the common Pushbot hardware class to define the devices on the robot.
+ * All device access is managed through the HardwarePushbot class.
+ * The code is structured as a LinearOpMode
+ * <p>
+ * This particular OpMode executes a POV Game style Teleop for a PushBot
+ * In this mode the left stick moves the robot FWD and back, the Right stick turns left and right.
+ * It raises and lowers the claw using the Gampad Y and A buttons respectively.
+ * It also opens and closes the claws slowly using the left and right Bumper buttons.
+ * <p>
+ * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
+ */
+
+@TeleOp(name = "Tele", group = "Pushbot")
+
+public class
+PerseverenceTeleop extends LinearOpMode {
+    HardwarePerseverence robot = new HardwarePerseverence();
+    private BNO055IMU imu;
+    private final ElapsedTime runtime = new ElapsedTime();
+    //    Transform2d cameraToRobot = new Transform2d();
+    public void waitMilis(double timeOutMs) {
+
+        runtime.reset();
+        while (runtime.milliseconds() < timeOutMs) ;
+    }
+
+
+    @Override
+    public void runOpMode() {
+        double leftFront;
+        double rightFront;
+        double leftBack;
+        double rightBack;
+        double drive;
+        double turn;
+        double max;
+        double r;
+        double robotAngle;
+        double driveSpeed;
+        // Increase this value to trust encoder odometry less when fusing encoder measurements with VSLAM
+        double encoderMeasurementCovariance = 0.8;
+
+        /* Initialize the hardware variables.
+         * The init() method of the hardware class does all the work here
+         */
+        robot.init(hardwareMap);
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.calibrationDataFile = "BNO055IMUCalibration.json"; // see the calibration sample opmode
+        parameters.loggingEnabled = true;
+        parameters.loggingTag = "IMU";
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+        imu.initialize(parameters);
+        //  Pose2d startingPose = new Pose2d(1, 1, new Rotation2d());
+        //  T265Camera slamra = new T265Camera(cameraToRobot, encoderMeasurementCovariance);
+        //reset the encoders to 0
+        robot.leftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.rightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.leftBackDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.rightBackDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.arm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        //run using the encoders
+        robot.leftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.rightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.leftBackDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.rightBackDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.arm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        // Send telemetry message to signify robot waiting;
+        telemetry.addData("Pushbot:", "Hello Driver");    //
+        telemetry.update();
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+            //  slamra.getLastReceivedCameraUpdate();
+            if (gamepad1.right_trigger > 50) {
+                driveSpeed = .5;
+            } else {
+                driveSpeed = 1;
+            }
+            double armCurr = robot.arm.getCurrentPosition();
+            double gripCurr = robot.grip.getPosition();
+            double rightX = gamepad1.right_stick_x;
+            r = Math.hypot(gamepad1.left_stick_x, gamepad1.left_stick_y);
+            robotAngle = Math.atan2(-gamepad1.left_stick_y, gamepad1.left_stick_x) - Math.PI / 4;
+            final double v1 = r * Math.cos(robotAngle) + rightX;
+            final double v2 = r * Math.sin(robotAngle) - rightX;
+            final double v3 = r * Math.sin(robotAngle) + rightX;
+            final double v4 = r * Math.cos(robotAngle) - rightX;
+            robot.leftDrive.setPower(v1 * driveSpeed);
+            robot.rightDrive.setPower(v2 * driveSpeed);
+            robot.leftBackDrive.setPower(v3 * driveSpeed);
+            robot.rightBackDrive.setPower(v4 * driveSpeed);
+
+            // Forks
+            if (gamepad2.b) { //open
+                robot.grip.setPosition(.65);
+                waitMilis(10);
+            } else if (gamepad2.a) { // closed
+                robot.grip.setPosition(.2);
+                waitMilis(10);
+            }
+            // Arm
+            robot.arm.setPower(gamepad2.left_stick_y * .75);
+
+
+            // Send telemetry message to signify robot running;
+            double  currentHeading = -imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES).firstAngle;
+            double headingRadians = -((-currentHeading / 180) * 3.1416) + (1 / 2 * 3.1416);
+            telemetry.addLine("Front Power");
+            telemetry.addData("Left Power", robot.leftDrive.getCurrentPosition());
+            telemetry.addData("Right Power", robot.rightDrive.getCurrentPosition());
+            telemetry.addLine("Back Power");
+            telemetry.addData("Left Power", robot.leftBackDrive.getCurrentPosition());
+            telemetry.addData("Right Power", robot.rightBackDrive.getCurrentPosition());
+            telemetry.addData("Arm", armCurr);
+            telemetry.addData("Grip", gripCurr);
+            telemetry.addLine("IMU");
+            telemetry.addData("Heading", headingRadians);
+            telemetry.update();
+        }
+    }
+}
